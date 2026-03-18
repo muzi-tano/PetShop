ee08724b0be3df1f66a698c109cc761bcc3bc92d
muzi-tano <muzitanoamatheus3@gmail.com>
2026-03-18 08:25:11 -0300

    Ajustando Menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
@@ -10,28 +12,31 @@ export const home = (req: Request, res: Response) => {
 };
 
 export const dogs = (req: Request, res: Response) => {
-  res.render('pages/page', {
-  banner: {
+    res.render('pages/page', {
+        banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
-        }
-});
-}
+        },
+        menu: createMenuObject('all'),
+    });
+};
 
 export const cats = (req: Request, res: Response) => {
-  res.render('pages/page', {
-  banner: {
+    res.render('pages/page', {
+        banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
-        }
-});
-}
+        },
+        menu: createMenuObject('all'),
+    });
+};
 
 export const fishes = (req: Request, res: Response) => {
-  res.render('pages/page', {
-  banner: {
+    res.render('pages/page', {
+        banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
-        }
-});
-}
+        },
+        menu: createMenuObject('all'),
+    });
+};
